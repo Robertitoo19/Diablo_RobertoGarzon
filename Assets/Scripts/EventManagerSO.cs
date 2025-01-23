bf4d5ac4012bf94d0006fb9214bd09b874406c14
Robertitoo19 <robertito191103@gmail.com>
2025-01-23 19:21:30 +0100

    fs

---
+++ Assets/Scripts/EventManagerSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Event Manager")]
+public class EventManagerSO : ScriptableObject
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
